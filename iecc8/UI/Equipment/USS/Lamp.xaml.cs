9d2ad5e402f24cdca6d66ad3c9cd7c05dd2c24a3
Emilia Connelly <83736062+ecgconnelly@users.noreply.github.com>
2022-11-23 23:23:18 -0800

    Set up color property

--- iecc8/UI/Equipment/USS/Lamp.xaml.cs
+++ iecc8/UI/Equipment/USS/Lamp.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +25,67 @@ namespace Iecc8.UI.Equipment.USS
         public Lamp()
         {
             InitializeComponent();
+        }
+
+        
+        private bool LampOn = true;
+
+        public void SetLampState(bool active)
+        {
+            this.LampOn = active;        
+        }
+
+        public void Update()
+        {
+            string source = LampColorProperty.ToString();
+            if (this.LampOn)
+            {
+                source += "-on";
+            }
+            else
+            {
+                source += "-off";
+            }
+
+            this.LampImage.Source = (ImageSource)FindResource(resourceKey: source);
+        }
+
+
+
+        public static DependencyProperty LampColorProperty = 
+            DependencyProperty.Register("LampColor", typeof(string), typeof(Lamp),
+                new PropertyMetadata("purple"));
+
+        [Category("Lamp"), Description("TEST TEST TEST")]
+        public string LampColor
+        {
+            get
+            {
+                return (string)GetValue(LampColorProperty);
+            }
+            set
+            {
+                SetValue(LampColorProperty, value);
+            }
+        }
+
+
+
+        public static DependencyProperty ImageSourceProperty =
+            DependencyProperty.Register("ImageSource", typeof(string), typeof(Lamp),
+                new PropertyMetadata("purple-on"));
+
+        [Category("Lamp"), Description("The default image source for design time use")]
+        public ImageSource ImageSource
+        {
+            get
+            {
+                return (ImageSource)GetValue(LampColorProperty);
+            }
+            set
+            {
+                SetValue(ImageSourceProperty, value);
+            }
         }
     }
 }
