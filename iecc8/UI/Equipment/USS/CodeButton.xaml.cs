fb543f44304831b2d0fc5108ff636aef14609a37
Emilia Connelly <83736062+ecgconnelly@users.noreply.github.com>
2022-11-25 17:19:06 -0800

    Link code buttons to switch and signal modules

--- iecc8/UI/Equipment/USS/CodeButton.xaml.cs
+++ iecc8/UI/Equipment/USS/CodeButton.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Iecc8.UI.Panels;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +23,86 @@ namespace Iecc8.UI.Equipment.USS
     /// </summary>
     public partial class CodeButton : UserControl
     {
+        public string SignalModulesStr
+        {
+            get
+            {
+                return (string)GetValue(SignalModulesStrProperty);
+            }
+            set
+            {
+                SetValue(SignalModulesStrProperty, value);
+            }
+        }
+        [Category("USS"), Description("Comma-separated list of module names this code button applies to")]
+        public static readonly DependencyProperty SignalModulesStrProperty =
+            DependencyProperty.Register(nameof(SignalModulesStr), typeof(string), typeof(SignalModule), new PropertyMetadata(""));
+
+        public string SwitchModulesStr
+        {
+            get
+            {
+                return (string)GetValue(SwitchModulesStrProperty);
+            }
+            set
+            {
+                SetValue(SwitchModulesStrProperty, value);
+            }
+        }
+        [Category("USS"), Description("Comma-separated list of module names this code button applies to")]
+        public static readonly DependencyProperty SwitchModulesStrProperty =
+            DependencyProperty.Register(nameof(SwitchModulesStr), typeof(string), typeof(SwitchModule), new PropertyMetadata(""));
+
+
         public CodeButton()
         {
             InitializeComponent();
+           
+        }
+
+        private void Press()
+        {
+
+        }
+
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Press(); //this can be expanded later
+        }
+
+        public List<SignalModule> signalModules { get; set; }
+        public List<SwitchModule> switchModules { get; set; }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (DesignerProperties.GetIsInDesignMode(this)) { return; }
+
+            //populate lists of signal and switch modules we code for
+            signalModules = new List<SignalModule>();
+            switchModules = new List<SwitchModule>();
+            foreach(string modname in SignalModulesStr.Split(','))
+            {
+                SignalModule mod = (SignalModule)LogicalTreeHelper.
+                    FindLogicalNode(Parent, modname);
+
+                if(mod != null)
+                {
+                    signalModules.Add(mod);
+                }
+            }
+
+            foreach (string modname in SwitchModulesStr.Split(','))
+            {
+                if (modname.Length == 0) { continue; }
+
+                SwitchModule mod = (SwitchModule)LogicalTreeHelper.
+                    FindLogicalNode(Parent, modname);
+
+                if (mod != null)
+                {
+                    switchModules.Add(mod);
+                }
+            }
         }
     }
 }
--- iecc8/UI/Panels/BakersfieldPanel.xaml.cs
+++ iecc8/UI/Panels/BakersfieldPanel.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Iecc8.UI.Equipment.USS;
+using Iecc8.UI.TopLevel;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +24,40 @@ namespace Iecc8.UI.Panels
     /// </summary>
     public partial class BakersfieldPanel : UserControl
     {
+
+     
+
+        public void GetLogicalTree(Object obj)
+        {
+            if (obj is FrameworkElement)
+            {
+                FrameworkElement fe = (FrameworkElement)obj;
+                if (fe.GetType() == typeof(Iecc8.UI.Equipment.USS.SignalModule))
+                    Console.WriteLine("SIGNAL MODULE: " + fe.GetType() + ", Name: " + fe.Name);
+                if (fe.GetType() == typeof(Iecc8.UI.Equipment.USS.SwitchModule))
+                    Console.WriteLine("SWITCH MODULE: " + fe.GetType() + ", Name: " + fe.Name);
+                // recurse through the children
+                IEnumerable children = LogicalTreeHelper.GetChildren(fe);
+                foreach (object child in children)
+                {
+                   GetLogicalTree(child);
+                }
+            }
+            else
+            {
+                // stop recursing as we certainly can't have any more FrameworkElement children
+                //Console.WriteLine("Logical Type: " + obj.GetType());
+            }
+        }
+
+
         public BakersfieldPanel()
         {
             InitializeComponent();
+
+            SignalModule sig4 = null;
+            sig4 = (SignalModule)LogicalTreeHelper.FindLogicalNode(this, "SignalModule4");
+            Debug.Print(sig4.CenterLampImage.Source.ToString());
         }
     }
 }
